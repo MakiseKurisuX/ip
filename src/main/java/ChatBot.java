7a1594482a788597b80d60c7d4a77768e10a86b4
Tan Zhang-Hng Ervin <58417787+MakiseKurisuX@users.noreply.github.com>
2025-01-28 02:35:29 +0800

    Update ChatBot.java
    Update ChatBot.java with Echo
    ----------------------------
    Level 1 Task for Week 2 iP

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class ChatBot {
   public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
     System.out.println("____________________________________________________________\r\n" + //
             " Hello! I'm Ervin Chatbot!\r\n" + //
             " What can I do for you?\r\n" + //
-            "____________________________________________________________\r\n" + //
-            " Bye. Hope to see you again soon!\r\n" + //
-            "____________________________________________________________");
-    
+            "____________________________________________________________\r\n");
+
+    while (true) {
+      String userMessage = in.nextLine();
+      if (userMessage.equals("bye")) {
+        System.out.println("____________________________________________________________\r\n" + //
+                "Bye. Hope to see you again soon!" + "\n" + //
+                "____________________________________________________________\r\n");
+        break;
+      }
+      System.out.println("____________________________________________________________\r\n" + //
+                userMessage + "\n" + //
+                "____________________________________________________________\r\n");
+    }
   }
 }
