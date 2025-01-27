7ad372a794684c5c7714aabb340bcb25884ec4c4
Tan Zhang-Hng Ervin <58417787+MakiseKurisuX@users.noreply.github.com>
2025-01-28 02:49:09 +0800

    Update ChatBot.java
    Update ChatBot.java with Add, List
    ----------------------------
    Level 2 Task for Week 2 iP

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ChatBot {
   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
+
+    ArrayList<String> tasks = new ArrayList<>();
 
     System.out.println("____________________________________________________________\r\n" + //
             " Hello! I'm Ervin Chatbot!\r\n" + //
@@ -17,10 +20,19 @@ public class ChatBot {
                 "Bye. Hope to see you again soon!" + "\n" + //
                 "____________________________________________________________\r\n");
         break;
-      }
-      System.out.println("____________________________________________________________\r\n" + //
-                userMessage + "\n" + //
+      } else if (userMessage.equals("list")) {
+        System.out.println("____________________________________________________________\r\n");
+        for (int i = 0; i < tasks.size(); i++) {
+          System.out.println((i+1) + ". " + tasks.get(i));
+        }
+        System.out.println("____________________________________________________________\r\n");
+      } else {
+        tasks.add(userMessage);
+        System.out.println("____________________________________________________________\r\n" + //
+                "added: " + userMessage + "\n" + //
                 "____________________________________________________________\r\n");
+      }
     }
+    
   }
 }
---
+++ src/main/java/task/Task.java
@@ -0,0 +1,17 @@
+package task;
+
+public class Task {
+  protected String description;
+  protected boolean isDone;
+
+  public Task(String description) {
+      this.description = description;
+      this.isDone = false;
+  }
+
+  public String getStatusIcon() {
+      return (isDone ? "X" : " "); // mark done task with X
+  }
+
+  //...
+}
