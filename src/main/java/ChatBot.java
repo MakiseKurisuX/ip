c57390347c1ca189ea1a35b5504f3e709e219ff5
Tan Zhang-Hng Ervin <58417787+MakiseKurisuX@users.noreply.github.com>
2025-01-28 02:29:03 +0800

    Add ChatBot.java
    Create ChatBot.java
    ----------------------------
    Level 0 Task for Week 2 iP

---
+++ src/main/java/ChatBot.java
@@ -0,0 +1,11 @@
+public class ChatBot {
+  public static void main(String[] args) {
+    System.out.println("____________________________________________________________\r\n" + //
+            " Hello! I'm Ervin Chatbot!\r\n" + //
+            " What can I do for you?\r\n" + //
+            "____________________________________________________________\r\n" + //
+            " Bye. Hope to see you again soon!\r\n" + //
+            "____________________________________________________________");
+    
+  }
+}
